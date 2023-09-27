fb5b4a5c7ecbe47bf2ff619a21675620430014e8
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-09-27 16:17:29 +0300

    Feature: Delete my user account

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,12 @@ import express from "express";
 import getUserProfile from "../controller/user/getUserProfile.js";
 import checkAuth from "../middleware/checkAuthMiddleware.js";
 import updateUserProfile from "../controller/user/updateUserProfile.js";
+import deleteMyAccount from "../controller/user/deleteMyAccount.js";
 
 const router = express.Router();
 
 router.get("/profile", checkAuth, getUserProfile);
 router.patch("/profile", checkAuth, updateUserProfile);
+router.delete("/profile", checkAuth, deleteMyAccount);
 
 export default router;
