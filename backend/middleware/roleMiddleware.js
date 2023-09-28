7e60720aef8064bd209f42e152fe16ffa6b8988b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-09-28 08:06:47 +0300

    feature:get all users accounts/profile admin only

--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -1,38 +1,34 @@
-import { ADMIN , USER } from "../constants/index.js";
+import { ADMIN, USER } from "../constants/index.js";
 
-const ROLES ={
-
-    User:USER, 
-    Admin:ADMIN,
-}
+const ROLES = {
+  User: USER,
+  Admin: ADMIN,
+};
 
 const checkRole = (...allowedRoles) => {
-    return (req, res, next) => {
-        if(!req?.user && !req?.roles){
-            restart.status(401);
+  return (req, res, next) => {
+    if (!req?.user && !req?.roles) {
+      restart.status(401);
 
+      throw new Error("you are not authorized on our platform");
+    }
 
-            throw new Error('you are not authorized on our platform')
-        }
+    const rolesArray = [...allowedRoles];
 
+    const roleFound = req.roles
+      .map((role) => rolesArray.includes(role))
+      .find((value) => value === true);
 
-        const rolesArray = [...allowedRoles];
+    if (!roleFound) {
+      res.status(401);
 
+      throw new Error("you are not authorized to perform  this request");
+    }
 
-        const roleFound = req.roles.map((role) => rolesArray.includes(role)).find((value) => value === true) ;
+    next();
+  };
+};
 
-        if(!roleFound){
-            res.status(401)
+const role = { ROLES, checkRole };
 
-            throw new Error('you are not authorized to perform  this request')
-
-        }
-
-        next()
-}
-
-}
-
-const role = {ROLES, checkRole}
-
-export default role
+export default role;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,11 +3,19 @@ import getUserProfile from "../controller/user/getUserProfile.js";
 import checkAuth from "../middleware/checkAuthMiddleware.js";
 import updateUserProfile from "../controller/user/updateUserProfile.js";
 import deleteMyAccount from "../controller/user/deleteMyAccount.js";
+import getAllUsersAccounts from "../controller/user/getAllUsersAccount.js";
+import role from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 
 router.get("/profile", checkAuth, getUserProfile);
 router.patch("/profile", checkAuth, updateUserProfile);
 router.delete("/profile", checkAuth, deleteMyAccount);
+router.get(
+  "/all",
+  checkAuth,
+  role.checkRole(role.ROLES.Admin),
+  getAllUsersAccounts
+);
 
 export default router;
